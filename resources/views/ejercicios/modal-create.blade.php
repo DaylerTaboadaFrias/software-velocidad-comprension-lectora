74a75050d7a473411a9a8206da231a1718a03fdc
Luis Almondory <almondoryluis@gmail.com>
2023-08-03 13:37:51 -0400

    Actualizar componente lecturas-editor

--- resources/views/ejercicios/modal-create.blade.php
+++ resources/views/ejercicios/modal-create.blade.php
@@ -32,7 +32,7 @@
                             <!-- Recomendaciones -->
                             <div class="relative mb-4">
                                 <label for="titulo"
-                                    class="leading-7 text-sm text-gray-600">{{ __('titulo') }}</label>
+                                    class="leading-7 text-sm text-gray-600">{{ __('Título') }}</label>
                                 <input type="text" id="titulo" name="titulo"
                                     value="{{ old('titulo') }}"
                                     class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
