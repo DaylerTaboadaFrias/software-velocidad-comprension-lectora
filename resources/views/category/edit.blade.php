dd8022ccd503d85a7d463fd8b2551bba530b854d
Luis Almondory <almondoryluis@gmail.com>
2023-08-08 08:51:53 -0400

    Actualizar vistas de Gestionar Categorías

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Editar nivel
+        Editar categoría
       </h2>
      
   </x-slot>
@@ -25,16 +25,16 @@
               @csrf
               @method('PUT')
               <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                  <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">{{ __("Escribe tu evento") }}</h2>
+                  <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">{{ __("Escribe tu categoría") }}</h2>
                   <div class="relative mb-4">
                       <label for="name" class="leading-7 text-sm text-gray-600">{{ __("Nombre") }}</label>
                       <input type="text" id="name" name="name" value="{{ old("nombre", $category->nombre) }}" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                   </div>
                   
                   <div class="relative mb-4">
-                      <label class="leading-7 text-sm text-gray-600" for="multiple_files">Elija una imagen para el nivel</label>
+                      <label class="leading-7 text-sm text-gray-600" for="multiple_files">Elija una imagen para la categoría</label>
                       <input name="img" class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="imagen" type="file">   
-                      <button type="submit" class=" mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar nivel</button>
+                      <button type="submit" class=" mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar categoría</button>
                   </div>
                   
               </div>
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Gestionar niveles') }}
+            {{ __('Gestionar categorías') }}
         </h2>
         <br>
         <a href="{{ route('category.create') }}" class=" mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">REGISTRAR</a>
